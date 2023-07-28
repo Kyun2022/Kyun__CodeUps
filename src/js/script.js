d0c8b245f12df2c626c94b26dd48687cd2f06e2a
kyun <fm13071303@rx.tnc.ne.jp>
2023-07-28 13:56:37 +0900

    一部修正をしました

--- src/js/script.js
+++ src/js/script.js
@@ -68,6 +68,17 @@ Common
     return false; //リンク自体の無効化
   });
 
+  // スクロールするとロゴの色変更
+  $(function () {
+    $(window).on("scroll", function () {
+      const sliderHeight = $(".mv").height();
+      if (sliderHeight - 30 < $(this).scrollTop()) {
+        $(".js-header").addClass("headerColorScroll");
+      } else {
+        $(".js-header").removeClass("headerColorScroll");
+      }
+    });
+  });
   /*//////////////////////////////////////
 Top
 /////////////////////////////////////*/
